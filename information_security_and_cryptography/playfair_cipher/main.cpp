5cb0a2ca4eadb2d75555fc9432dee7206d765a82
wh0-1-am <o.t5@hotmail.com>
2018-02-12 20:48:04 +0300

    add folder playfair

---
+++ information_security_and_cryptography/playfair_cipher/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: MacBookPro
+ *
+ * Created on 12 February 2018, 20:47
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
