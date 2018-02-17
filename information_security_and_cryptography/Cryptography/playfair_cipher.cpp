c0efb5c7a8ebe8de4ace9c25ce9f0d619a1c2bd2
The-7-Hunter <nawafhunter@gmail.com>
2018-02-17 20:36:04 +0300

    creating Playfair cipher file
    setting up the basics :)

---
+++ information_security_and_cryptography/Cryptography/playfair_cipher.cpp
@@ -0,0 +1,50 @@
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+//
+// Copyright            : (C) 2017 The Team Extream
+// File name            : Playfair-cipher
+//
+// -------------------------------------------------------------------------
+// A
+//              _____           _
+//             /  __ \         | |
+//             | /  \/ ___   __| | ___
+//             | |    / _ \ / _  |/ _ \
+//             | \__/\ (_) | (_| |  __/
+//              \____/\___/ \__,_|\____
+//
+//                                                  F i l e
+//
+//    This program is what we will use to show the 'Playfair Cipher' encryption / decryption.
+//    Created by:
+//    -Wayfarer
+//    -the-7-hunter
+//     comments
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void Playfair_Encrypt(string msg,string key);
+void Playfair_Decrypt(string msg);
+
+
+int main(){
+
+string msg,key;
+
+
+cout<<">>>Welcome to Playfair Cipher <<<"<<endl;
+cout<<"Please choose (1) for encryption, (2) for decryption .. "<<endl;
+cout<<"---------------------------------------------"<<endl;<<endl;
+
+
+
+
+
+
+
+}
